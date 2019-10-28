34527e0d478ec66813ad1cdb638a16d4d4c214f1
fadilradit <fadilradit@gmail.com>
2019-10-28 19:19:54 +0700

    first

---
+++ src/component/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {BrowserRouter} from 'react-router-dom'
+
+class App extends Component{
+
+    render(){
+        return(
+            <BrowserRouter>
+                
+            </BrowserRouter>
+        )
+    }
+}
+
+export default App;
